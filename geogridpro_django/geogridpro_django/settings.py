d06974096bea24d5fa688a5b22226a4e34dab38e
GrigorySuturin <crack300@yandex.ru>
2025-01-12 11:30:40 +0700

    Add database

--- geogridpro_django/geogridpro_django/settings.py
+++ geogridpro_django/geogridpro_django/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'geogridpro_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geogridpro_db',
+        'USER': 'postgres',
+        'PASSWORD': 'bfrfgrgrgg2002',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
